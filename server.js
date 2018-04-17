0eb0d3b072037261a5be64979f897717e80607ef
Haoran Zhang <haoranzhang@icloud.com>
2018-04-17 11:41:43 +0100

    Creating User Model

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ app.use("/api/posts", posts);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () =>
-  console.log(`Server runing on: http://localhost:${port}`)
+  console.log(`Server runing on http://localhost:${port}`)
 );
