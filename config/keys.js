17dd2ece8cadac9b0d4e16d7d7533b708065b3b0
Haoran Zhang <haoranzhang@icloud.com>
2018-04-17 15:06:57 +0100

    Creating JWT

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://zhangha:zhangha@ds247439.mlab.com:47439/mernproject"
+  mongoURI: "mongodb://zhangha:zhangha@ds247439.mlab.com:47439/mernproject",
+  sercretOrKey: "secret"
 };
