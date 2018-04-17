09c129a3b0b860b8e368381625c1716bff1eb607
Haoran Zhang <haoranzhang@icloud.com>
2018-04-17 15:35:00 +0100

    Passport JWT Authentication Strategy

---
+++ config/passport.js
@@ -0,0 +1,25 @@
+const JWTStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+const keys = require("./keys");
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.sercretOrKey;
+
+module.exports = passport => {
+  passport.use(
+    new JWTStrategy(opts, (jwt_payload, done) => {
+      console.log(jwt_payload);
+      User.findById(jwt_payload.id)
+        .then(user => {
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
+        })
+        .catch(err => console.log(err));
+    })
+  );
+};
