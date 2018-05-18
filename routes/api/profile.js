80a0510c9bbe8f4fcacc5a280d83fcb037f31437
Haoran Zhang <haoranzhang@icloud.com>
2018-05-18 12:41:37 +0100

    Current User Profile Route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load Profile Model
+const Profile = require("../../models/Profile");
+// Load User Profile
+const User = require("../../models/User");
 
 // @route   GET api/profile/test
 // @desc    Tests profile route
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Profile works!" }));
+
+// @route   GET api/profile
+// @desc    Get current users profile
+// @access  Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errs = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errs.noprofile = "There is no profile for this user";
+          return res.status(404).json(errs);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
